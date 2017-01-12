95b3cc4ca73ec38e5725618ea7e4a8cf19a4263e
ZhuzhuXingTianXia <873391579@qq.com>
2017-01-12 15:03:05 +0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,6 +1,21 @@
 //
 autoConvertOCType(1);
-defineClass('ViewController',{
+require('UIButton,UIColor');
+defineClass('ViewController', {
+    bulidView: function() {
+        var btn = UIButton.buttonWithType(UIButtonTypeSystem);
+        btn.setBounds(CGRectMake(0, 0, 80, 40));
+        btn.setCenter(self.view().center());
+        btn.setTitle_forState("谁是大神", UIControlStateNormal);
+
+        btn.setTitleColor_forState(UIColor.whiteColor(), UIControlStateNormal);
+        btn.setBackgroundColor(UIColor.colorWithRed_green_blue_alpha(80 / 255.0, 140 / 255.0, 238 / 255.0, 1.0));
+        btn.layer().setCornerRadius(5.0);
+        btn.layer().setMasksToBounds(YES);
+
+        btn.addTarget_action_forControlEvents(self, @selector(jsScriptRun: ), UIControlEventTouchUpInside);
+        self.view().addSubview(btn);
+    },
             jsScriptRun:function(obc){
             console.log(obc);
             var alertView = require('UIAlertView').alloc().init();
@@ -10,4 +25,5 @@ defineClass('ViewController',{
             alertView.addButtonWithTitle('必须是');
             alertView.show();
             }
-})
+
+});
