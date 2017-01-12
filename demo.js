c91fb3c851c0eae7fed52c0e57a681abf314d12f
ZhuzhuXingTianXia <873391579@qq.com>
2017-01-12 14:52:35 +0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -4,9 +4,10 @@ defineClass('ViewController',{
             jsScriptRun:function(obc){
             console.log(obc);
             var alertView = require('UIAlertView').alloc().init();
-            alertView.setTitle('Alert');
-            alertView.setMessage('罗老师是大神');
+            alertView.setTitle('贾老师你说呢');
+            alertView.setMessage('罗老师是不是大神');
             alertView.addButtonWithTitle('是的');
+            alertView.addButtonWithTitle('必须是');
             alertView.show();
             }
 })
